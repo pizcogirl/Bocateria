abcf21dc6fa3ef97eb389102bf0f36d26cf19fd5
Julia Zuara <julia_any@hotmail.com>
2015-03-13 12:09:57 +0100

    Codificado el metodo llegaNuevoClienteALaCola

--- Bocateria.java
+++ Bocateria.java
@@ -1,4 +1,5 @@
-import java.util.HashMap
+import java.util.HashMap;
+
 /**
  * Representa una bocateria
  * 
@@ -21,8 +22,36 @@ public class Bocateria
      */
     public Bocateria()
     {
-        // initialise instance variables
-        x = 0;
+        // Inicializamos las variables, no tenemos primer cliente aun, la facturacion sera 0
+        primeraPersonaEnCola = null;
+        facturacionActual = 0;
+        // Inicializamos el hashmap
+        clientesDespachados = new HashMap<Integer, Cliente>();
+    }
+
+    /**
+     * Metodo que coloca un nuevo cliente en la cola
+     */
+    public void llegaNuevoClienteALaCola(int numeroDeBocadillos)
+    {
+        // Primero comprobamos si existe algun cliente en la cola
+        if (primeraPersonaEnCola == null)
+        {
+            // Si no hay ninguno, lo colocamos el primero
+            primeraPersonaEnCola = new Cliente(numeroDeBocadillos);
+        }
+        else
+        {
+            // Si ya existen, lo colocamos en la cola, para ello cojemos el siguiente en la cola
+            // del primer cliente, y vamos preguntando hasta llegar al ultimo
+            Cliente ultimoCola = primeraPersonaEnCola;
+            while(ultimoCola.getSiguienteEnLaCola() != null)
+            {
+                ultimoCola = ultimoCola.getSiguienteEnLaCola();
+            }
+            // CUando llegamos al que no tiene nadie detras, seteamos su siguiente en la cola con el nuevo cliente
+            ultimoCola.setSiguienteEnLaCola(new Cliente(numeroDeBocadillos));
+        }
     }
 
 }
